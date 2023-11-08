932f9a56565e5370cf0e03baab551c3fbf46507c
jack-ubuntu <jackw1689@gmail.com>
2023-11-08 14:47:29 -0800

    updated timeouts

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ async def respond():
        Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
        """
        # send the welcoming message
-       await bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+       await bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id,read_timeout=10, write_timeout=10, connect_timeout=10, pool_timeout=10)
 
 
    else:
@@ -51,7 +51,7 @@ async def respond():
            url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
            # reply with a photo to the name the user sent,
            # note that you can send photos by url and telegram will fetch it for you
-           await bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
+           await bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id,read_timeout=10, write_timeout=20, connect_timeout=10, pool_timeout=10)
        except Exception:
            # if things went wrong
            await bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
